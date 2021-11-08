631667af1ad3d6d200feb7995f309862dd0c24cf
Zefanja Jobse <zefanjaj@gmail.com>
2021-11-08 10:14:01 +0100

    hello string :p

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello World");
+        String hello = "Hello World";
+        System.out.println(hello);
     }
 }
